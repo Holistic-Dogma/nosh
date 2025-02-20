a07d7c73d02848c6b8d5df6f0e7fbd5309c90e87
The Fall of Man <developer@iswork.in>
2025-02-20 05:18:53 -0800

    chore: update nosh template

--- .nosh/etc/template/app/src/api/index.js
+++ .nosh/etc/template/app/src/api/index.js
@@ -1,8 +1,12 @@
-import { UserAuth } from './user/auth'
+//import { UserAuth } from './routes/user/auth'
+import { Test } from './routes/test/suite'
 const routes = []
 const R = (path, options = { method: 'get'}) => {
   const method = options.method ?? 'get'
   routes.push({ ...options, method, path })
 }
 
-R('users/auth', UserAuth.validate)
+//R('users/auth', UserAuth.validate)
+R('rev/:term', Test.backwards)
+R('reverse/:term', Test.backwards)
+export { routes }
